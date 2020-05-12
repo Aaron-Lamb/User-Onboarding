3d8861ed4e55618d814fcb8aae887a2a4676644a
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-12 09:13:21 -0700

    Created the components folder, with one UserForm component. Within that form I imported React, * as yup, and axios. So far I am just returning a form, which I have imported into App and am currently displaying.

---
+++ user-app/src/components/UserForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const UserForm = () => {
+    return(
+        <form>
+            
+        </form>
+    )
+}
+
+export default UserForm;
