ef15af4fe4b8921d499aeff7c6169c6dcab990f3
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-12 16:43:58 -0700

    Created state for the form with the appropriate key value pairs. Wrote the userChange function and passed it as onchange to all of the inputs.

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -3,23 +3,39 @@ import * as yup from 'yup';
 import axios from 'axios';
 
 const UserForm = () => {
+
+    const [userState, setUserState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        terms: false
+    })
+
+    const userChange = (event) => {
+        let value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        setUserState({
+            ...userState,
+            [event.target.name]: value
+        });
+    }
+
     return(
         <form>
             <label>
                 Name: 
-                <input type='text' name='name' id='name' />
+                <input type='text' name='name' id='name' value={userState.name} onChange={userChange} />
             </label>
             <label>
                 Email: 
-                <input type='email' name='email' id='email' />
+                <input type='email' name='email' id='email' value={userState.email} onChange={userChange} />
             </label>
             <label>
                 Password: 
-                <input type='password' name='password' id='password' />
+                <input type='password' name='password' id='password' value={userState.password} onChange={userChange} />
             </label>
             <label>
                 Do you agree to the terms of service?
-                <input type='checkbox' name='terms' id='terms' checked/>
+                <input type='checkbox' name='terms' id='terms' checked={userState.terms} onChange={userChange} />
             </label>
             <button type='submit'>Submit User</button>
         </form>
