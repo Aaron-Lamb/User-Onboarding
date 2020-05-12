252e04b9d96034bcd80b4d7a1a7fe6baccf37729
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-12 16:27:01 -0700

    Added four labels with corresponding inputs to my form, each with a type, name and id, and the checkbox having the attribute of checked.

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -5,7 +5,22 @@ import axios from 'axios';
 const UserForm = () => {
     return(
         <form>
-            
+            <label>
+                Name: 
+                <input type='text' name='name' id='name' />
+            </label>
+            <label>
+                Email: 
+                <input type='email' name='email' id='email' />
+            </label>
+            <label>
+                Password: 
+                <input type='password' name='password' id='password' />
+            </label>
+            <label>
+                Do you agree to the terms of service?
+                <input type='checkbox' name='terms' id='terms' checked/>
+            </label>
         </form>
     )
 }
